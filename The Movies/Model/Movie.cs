8b269b577c2fe09c14f8b2d13954a8afe626ead5
A-Iversen <abiv71341@edu.ucl.dk>
2025-08-14 13:36:33 +0200

    Repository Update

--- The Movies/Model/Movie.cs
+++ The Movies/Model/Movie.cs
@@ -8,8 +8,38 @@ namespace The_Movies.Model
 {
     public class Movie
     {
-        public string Title { get; set; }
-        public double Duration { get; set; }
-        public string Genre { get; set; }
+        // Properties
+        private string _title;
+        private double _duration;
+        private string _genre;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public double Duration
+        {
+            get { return _duration; }
+            set { _duration = value; }
+        }
+        public string Genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
+
+        // Constructor
+        public Movie(string title, double duration, string genre)
+        {
+            _title = title;
+            _duration = duration;
+            _genre = genre;
+        }
+
+        // Methods
+
     }
+
+    
 }
---
+++ The Movies/Repository/FileMovieRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using The_Movies.Model;
+
+namespace The_Movies.Repository
+{
+    public class FileMovieRepository
+    {
+        // filepath
+        private string _filePath;
+        public List<Movie> movieList;
+
+        // Constructor
+        public FileMovieRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+        // Methods to read and write movies to the file
+        public void AddMovie(Movie movie)
+        {
+            movieList.Add(movie);
+
+            using var writer = new System.IO.StreamWriter(_filePath, true);
+            writer.WriteLine($"{movie.Title},{movie.Duration},{movie.Genre}");
+        }
+
+    }
+}
