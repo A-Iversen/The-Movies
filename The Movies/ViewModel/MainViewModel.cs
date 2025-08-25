6f9968739a6ff46b6891b76dd0079a7780292708
Ninna Lundberg <nllu71404@edu.ucl.dk>
2025-08-25 23:52:08 +0200

    MainViewModel oprettet

---
+++ The Movies/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using The_Movies.Repository;
+using The_Movies.ViewModel;
+
+namespace The_Movies.ViewModel
+{
+   public class MainViewModel
+    {
+
+        public MovieViewModel MovieVM { get; }
+        public ShowViewModel ShowVM { get; }
+
+        public MainViewModel()
+        {
+            MovieVM = new MovieViewModel();
+            ShowVM = new ShowViewModel();
+        }
+
+
+
+    }
+}
