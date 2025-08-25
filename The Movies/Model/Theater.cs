9a191944cec744cbc5eb74dab0954d1f43d2669e
Mikkel Simon Ølting Jensen <msoj71393@edu.ucl.dk>
2025-08-25 13:59:16 +0200

    Added a class call Theater that holds theater name and it also holds Halls, so we don't need the Enum Halls anymore.

---
+++ The Movies/Model/Theater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Movies.Model
+{
+    internal class Theater
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<string> Halls { get; set; } = new();
+        public override string ToString() { return Name; }
+    }
+}
